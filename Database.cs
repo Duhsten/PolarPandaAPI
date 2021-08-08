6f26f7be9af7432f205830165e506a5b69c056df
Dustin Osborne <dustinkosborne@gmail.com>
2021-08-08 04:07:04 -0400

    +

--- Database.cs
+++ Database.cs
@@ -125,17 +125,17 @@ namespace PolarPandaWebAPI {
           string sqlString = "SET ";
           if(displayName != "" && displayName != "null")
           {
-              sqlString = sqlString + "'display_name' = '" + displayName + "', ";
+              sqlString = sqlString + "display_name = '" + displayName + "', ";
           }
           if(avatar_url != "" && avatar_url != "null")
           {
-              sqlString = sqlString + "'avatar_url' = '" + avatar_url + "', ";
+              sqlString = sqlString + "avatar_url = '" + avatar_url + "', ";
           }
          if(gold != plyInfo.gold)
           {
-              sqlString = sqlString + "'gold' = '" + gold + "'";
+              sqlString = sqlString + "gold = " + gold + "";
           }
-
+            sqlString.TrimEnd(' ', ',' , '<');
                  CloseConnection();
         OpenConnection();
         Console.WriteLine(sqlString);
