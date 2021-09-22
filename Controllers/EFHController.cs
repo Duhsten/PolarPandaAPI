4a5e444627a9952ee08ac406e4082bc22fdf45e3
Dustin Osborne <dustinkosborne@gmail.com>
2021-09-22 18:53:20 -0400

    +

--- Controllers/EFHController.cs
+++ Controllers/EFHController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+using System.IO;
 namespace PolarPandaWebAPI.Controllers
 {
     [ApiController]
@@ -19,12 +22,41 @@ namespace PolarPandaWebAPI.Controllers
             _logger = logger;
         }
         [HttpGet("check")]
-        public bool status(int id)
+        public String status(int id)
         {
-           return true;
+           string text = System.IO.File.ReadAllText(@"access");
+           if(text.Equals("true"))
+           {
+              return "true"; 
+           }
+           else if(text.Equals("false"))
+           {
+              return "lock"; 
+           }
+           else
+           {
+               return "false"; 
+           }
             
         }
+    [HttpGet("retrievelist")]
+        public HttpResponseMessage GetFile(string id)
+        {
 
-        
+            string fileName;
+            string localFilePath;
+            int fileSize;
+
+            localFilePath = "test.zip";
+            fileName = "test.zip";
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new StreamContent(new FileStream(localFilePath, FileMode.Open, FileAccess.Read));
+            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = fileName;
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            return response;
+}
     }
 }
